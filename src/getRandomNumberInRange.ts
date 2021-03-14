f00c237bb44016612983d44e998816984eb1a78a
Joeri Bakker <bakkerjoeri@gmail.com>
2021-03-14 21:37:34 +0100

    Li'l fix

--- src/getRandomNumberInRange.ts
+++ src/getRandomNumberInRange.ts
@@ -1,6 +1,6 @@
 export function getRandomNumberInRange(
-    lowerBound: number,
-    upperBound: number
+    min: number,
+    max: number
 ): number {
-	return Math.floor(Math.random() * (upperBound + 1 + lowerBound))
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
